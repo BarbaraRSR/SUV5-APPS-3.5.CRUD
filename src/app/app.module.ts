1d765fc87113a6ee9870d474efd24bace3ab07b9
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-04-30 18:23:35 -0500

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { HttpClientModule } from '@angular/common/http';
+import {Route, RouterModule} from '@angular/router';
+const routes: Route[] = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'form', component: FormComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
