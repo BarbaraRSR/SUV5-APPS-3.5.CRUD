f051f7f93364dc8570666b1d4792b745dae255d7
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-04-30 19:37:15 -0500

    Add files via upload

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Movie } from '../interfaces/movie';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MoviesService } from '../services/movies.service';
 
 @Component({
   selector: 'app-form',
@@ -7,20 +8,22 @@ import { Movie } from '../interfaces/movie';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  movie: Movie = {
-    title: null,
-    synopsis: null,
-    year: null,
-    cover: null
-  };
 
-  constructor() { }
+  constructor (private movie:MoviesService) {  }
+  addMovie = new FormGroup({
+    title: new FormControl( '' ),
+    synopsis: new FormControl ( '' ),
+    year: new FormControl ( '' ),
+    cover: new FormControl ( '' )
+  });
 
   ngOnInit(): void {
   }
 
-  saveMovie() {
-    console.log(this.movie);
+  SaveData(){
+    this.movie.saveMovieData( this.addMovie.value ).subscribe((result) => {
+      console.log(result);
+    });
   }
 
 }
