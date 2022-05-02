1514ea90648c3854b29159a8b1a1f142f3135e7c
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-05-02 13:15:26 -0500

    WARNING!
    Guardado con errores.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../interfaces/movie';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { MoviesService } from '../services/movies.service';
 
 @Component({
@@ -8,22 +10,42 @@ import { MoviesService } from '../services/movies.service';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
-  constructor (private movie:MoviesService) {  }
   addMovie = new FormGroup({
     title: new FormControl( '' ),
     synopsis: new FormControl ( '' ),
     year: new FormControl ( '' ),
     cover: new FormControl ( '' )
   });
+  message:boolean = false;
+  id: any;
+  edit: boolean = false;
+  movies: Movie[];
+
+  constructor (private movie:MoviesService, private activatedRoute: ActivatedRoute) { 
+    this.id = this.activatedRoute.snapshot.params['id'];
+    if (this.id) {
+      this.edit = true;
+      this.moviesService.get().subscribe( (data: Movie[]) => {
+        this.movies = data;
+        console.log(data);
+      });
+    } else {
+      this.edit = false;
+    }
+  }
 
   ngOnInit(): void {
   }
 
   SaveData(){
     this.movie.saveMovieData( this.addMovie.value ).subscribe((result) => {
-      console.log(result);
+      // console.log(result);
+      this.message = true;
+      this.addMovie.reset( {} );
     });
+  }
+  xmessage(){
+    this.message = false;
   }
 
 }
