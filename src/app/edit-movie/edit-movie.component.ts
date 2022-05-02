ce84ba37ef9bcfcc27192b0d041b0f63a7a5d626
BarbaraRSR <79948777+BarbaraRSR@users.noreply.github.com>
2022-05-02 17:34:26 -0500

    Add files via upload

--- src/app/edit-movie/edit-movie.component.ts
+++ src/app/edit-movie/edit-movie.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MoviesService } from '../services/movies.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-edit-movie',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditMovieComponent implements OnInit {
 
-  constructor() { }
+  constructor( private movie: MoviesService, private router: ActivatedRoute ) { }
+
+  editMovie = new FormGroup( {
+    title: new FormControl( '' ),
+    synopsis: new FormControl ( '' ),
+    year: new FormControl ( '' )
+  } );
+
+  message:boolean = false;
 
   ngOnInit(): void {
+    this.movie.getOneMovie( this.router.snapshot.params['id'] ).subscribe( (result: any) => {
+      this.editMovie = new FormGroup( {
+        title: new FormControl( result ['title'] ),
+        synopsis: new FormControl ( result ['synopsis'] ),
+        year: new FormControl ( result ['year'] )
+      } );
+    });
+  }
+
+  EditData() {
+    this.movie.updateMovie( this.router.snapshot.params['id'], this.editMovie.value ).subscribe( (result) => {
+      this.message = true;
+    })
+  }
+
+  xmessage(){
+    this.message = false;
   }
 
 }
